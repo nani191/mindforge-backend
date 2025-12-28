d0d1189fac4b0e370dd26ae462de9c1797a44951
nani191 <yasalapun@gmail.com>
2025-12-28 11:10:50 +0530

    chore: define base backend folder structure

---
+++ src/test/java/com/mindforge/backend/MindforgeBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mindforge.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MindforgeBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
